d3cbf8ad62706e3dba93eedcd925d03196879770
oeykmiih <mail@kyeo.xyz>
2023-09-12 21:37:44 +0200

    interface: add header redraw funcitons,
    in future they will replace native headers (toggle-able)

--- src/interface/headers.py
+++ src/interface/headers.py
@@ -83,19 +83,7 @@ class ARK_MT_PIE_SetEditorMode(bpy.types.Menu):
             )
             operator.ui_type = ui_type
         return None
-def ARK_BT_NODE_SetEditorType(self, context):
-    ui_type = 'VIEW_3D'
-    operator = self.layout.operator(
-        ARK_OT_QuickEditorType.bl_idname,
-        text="",
-        icon=enums.EDITOR_TYPE_ICONS[ui_type],
-    )
-    operator.ui_type = ui_type
-    return None
 
-def ARK_BT_OUTLINER_SetEditorType(self, context):
-    ui_type = 'PROPERTIES'
-    operator = self.layout.operator(
 class ARK_OT_VIEW3D_ZoomExtents(bpy.types.Operator):
     bl_idname = f"{addon.name}.zoom_extents"
     bl_label = ""
@@ -112,32 +100,221 @@ class ARK_OT_VIEW3D_ZoomExtents(bpy.types.Operator):
         else:
             bpy.ops.view3d.view_center_camera()
         return {"INTERFACE"}
-        ARK_OT_QuickEditorType.bl_idname,
-        text="",
-        icon=enums.EDITOR_TYPE_ICONS[ui_type],
-    )
-    operator.ui_type = ui_type
-    # self.layout.separator()
-    return None
 
-def ARK_BT_PROPERTIES_SetEditorType(self, context):
+def ARK_OUTLINER_HT_draw(self, context):
+    layout = self.layout
+
+    bt = layout.operator(
+        ARK_OT_QuickEditorType.bl_idname,
+        text = "",
+        icon = enums.EDITOR_TYPE_ICONS['PROPERTIES'],
+    )
+    bt.ui_type = 'PROPERTIES'
+
+    row = layout.row(align=True)
     ui_type = 'OUTLINER'
-    operator = self.layout.operator(
-        ARK_OT_QuickEditorType.bl_idname,
-        text="",
-        icon=enums.EDITOR_TYPE_ICONS[ui_type],
+    ui_mode = 'VIEW_LAYER'
+    bt = row.operator(
+        ARK_OT_SetEditorMode.bl_idname,
+        text = "",
+        icon = enums.EDITOR_MODE_ICONS[ui_type][ui_mode],
+        depress = utils.blpy.validate_props(enums.EDITOR_MODE[ui_type][ui_mode]),
     )
-    operator.ui_type = ui_type
-    # self.layout.separator()
+    bt.ui_type = ui_type
+    bt.ui_mode = ui_mode
+
+    ui_mode = 'FILE'
+    bt = row.operator(
+        ARK_OT_SetEditorMode.bl_idname,
+        text = "",
+        icon = enums.EDITOR_MODE_ICONS[ui_type][ui_mode],
+        depress = utils.blpy.validate_props(enums.EDITOR_MODE[ui_type][ui_mode]),
+    )
+    bt.ui_type = ui_type
+    bt.ui_mode = ui_mode
+
+    ui_mode = 'FILE_MATERIAL'
+    bt = row.operator(
+        ARK_OT_SetEditorMode.bl_idname,
+        text = "",
+        icon = enums.EDITOR_MODE_ICONS[ui_type][ui_mode],
+        depress = utils.blpy.validate_props(enums.EDITOR_MODE[ui_type][ui_mode]),
+    )
+    bt.ui_type = ui_type
+    bt.ui_mode = ui_mode
+
+    ui_mode = 'FILE_NODETREE'
+    bt = row.operator(
+        ARK_OT_SetEditorMode.bl_idname,
+        text = "",
+        icon = enums.EDITOR_MODE_ICONS[ui_type][ui_mode],
+        depress = utils.blpy.validate_props(enums.EDITOR_MODE[ui_type][ui_mode]),
+    )
+    bt.ui_type = ui_type
+    bt.ui_mode = ui_mode
+
+    layout.separator()
+
+    row = layout.row(align=True)
+    bt = row.operator(
+        "outliner.orphans_purge",
+        text = "Purge",
+    )
+    bt.do_recursive = True
+
+    layout.separator()
+
+    row = layout.row(align=True)
+    ui_mode = 'ORPHAN'
+    bt = row.operator(
+        ARK_OT_SetEditorMode.bl_idname,
+        text = "",
+        icon = enums.EDITOR_MODE_ICONS[ui_type][ui_mode],
+        depress = utils.blpy.validate_props(enums.EDITOR_MODE[ui_type][ui_mode]),
+    )
+    bt.ui_type = ui_type
+    bt.ui_mode = ui_mode
+
+    ui_mode = 'ORPHAN_MATERIAL'
+    bt = row.operator(
+        ARK_OT_SetEditorMode.bl_idname,
+        text = "",
+        icon = enums.EDITOR_MODE_ICONS[ui_type][ui_mode],
+        depress = utils.blpy.validate_props(enums.EDITOR_MODE[ui_type][ui_mode]),
+    )
+    bt.ui_type = ui_type
+    bt.ui_mode = ui_mode
+
+    ui_mode = 'ORPHAN_NODETREE'
+    bt = row.operator(
+        ARK_OT_SetEditorMode.bl_idname,
+        text = "",
+        icon = enums.EDITOR_MODE_ICONS[ui_type][ui_mode],
+        depress = utils.blpy.validate_props(enums.EDITOR_MODE[ui_type][ui_mode]),
+    )
+    bt.ui_type = ui_type
+    bt.ui_mode = ui_mode
+
+    layout.separator_spacer()
     return None
 
-def ARK_BT_VIEW3D_SetEditorType(self, context):
-    ui_type = 'ShaderNodeTree'
-    operator = self.layout.operator(
+def ARK_VIEW3D_HT_draw(self, context):
+    layout = self.layout
+
+    bt = layout.operator(
         ARK_OT_QuickEditorType.bl_idname,
-        text="",
-        icon=enums.EDITOR_TYPE_ICONS[ui_type],
+        text = "",
+        icon = enums.EDITOR_TYPE_ICONS['ShaderNodeTree'],
     )
-    operator.ui_type = ui_type
-    # self.layout.separator()
+    bt.ui_type = 'ShaderNodeTree'
+
+    row = layout.row(align=True)
+    ui_type = 'VIEW_3D'
+    ui_mode = 'WIREFRAME'
+    bt = row.operator(
+        ARK_OT_SetEditorMode.bl_idname,
+        text = "",
+        icon = enums.EDITOR_MODE_ICONS[ui_type][ui_mode],
+        depress = utils.blpy.validate_props(enums.EDITOR_MODE[ui_type][ui_mode])
+    )
+    bt.ui_type = ui_type
+    bt.ui_mode = ui_mode
+
+    ui_mode = 'SOLID'
+    bt = row.operator(
+        ARK_OT_SetEditorMode.bl_idname,
+        text = "",
+        icon = enums.EDITOR_MODE_ICONS[ui_type][ui_mode],
+        depress = utils.blpy.validate_props(enums.EDITOR_MODE[ui_type][ui_mode])
+    )
+    bt.ui_type = ui_type
+    bt.ui_mode = ui_mode
+
+    ui_mode = 'RENDERED'
+    bt = row.operator(
+        ARK_OT_SetEditorMode.bl_idname,
+        text = "",
+        icon = enums.EDITOR_MODE_ICONS[ui_type][ui_mode],
+        depress = utils.blpy.validate_props(enums.EDITOR_MODE[ui_type][ui_mode])
+    )
+    bt.ui_type = ui_type
+    bt.ui_mode = ui_mode
+
+    row = layout.row(align=True)
+    bt = row.operator(
+        "view3d.view_selected",
+        text = "",
+        icon = 'ZOOM_IN',
+    )
+
+    bt = row.operator(
+        ARK_OT_VIEW3D_ZoomExtents.bl_idname,
+        text = "",
+        icon = 'VIEW_ZOOM',
+    )
+
+    bt = row.operator(
+        "view3d.view_camera",
+        text = "",
+        icon = 'CAMERA_DATA',
+        depress = (context.area.spaces.active.region_3d.view_perspective == 'CAMERA'),
+        )
+
+    row = layout.row(align=True)
+    bt = row.prop(
+        context.space_data.overlay,
+        "show_outline_selected",
+        text = "",
+        icon = 'MESH_CUBE',
+    )
+
+    row = layout.row(align=True)
+    row.prop(context.scene.render,
+        "film_transparent",
+        text = "",
+        icon = 'WORLD',
+    )
+
+    sub = row.row(align=True)
+    sub.prop(context.scene.render,
+        "use_border",
+        text = "",
+        icon = 'SHADING_BBOX',
+    )
+
+    if context.scene.camera:
+        sub.prop(
+            context.scene.camera.data,
+            "show_passepartout",
+            text = "",
+            icon = 'CAMERA_STEREO',
+        )
+    else:
+        sub.operator(
+            utils.UTILS_OT_Placeholder.bl_idname,
+            text = "",
+            icon = 'CAMERA_STEREO',
+        )
+        sub.enabled = False
+
+    row = layout.row(align=True)
+    object_mode = 'OBJECT' if context.active_object is None else context.active_object.mode
+    if object_mode not in {'OBJECT', 'EDIT'}:
+        row.enabled = False
+    row.prop(
+        context.tool_settings,
+        "transform_pivot_point",
+        text = "",
+        icon_only = True,
+    )
+
+    row.prop_with_popover(
+        context.scene.transform_orientation_slots[0],
+        "type",
+        text = "",
+        panel = "VIEW3D_PT_transform_orientations",
+        icon_only = True,
+    )
+
+    layout.separator_spacer()
     return None
