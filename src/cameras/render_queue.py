c3c655070611b5505b3cdd3b8388966e9fea68b4
oeykmiih <mail@kyeo.xyz>
2023-10-01 09:46:19 +0200

    refactor(render_queue): remove functions not used in submodules

--- src/cameras/render_queue.py
+++ src/cameras/render_queue.py
@@ -12,26 +12,15 @@ class ARK_WindowManager_Cameras_RenderQueue(bpy.types.PropertyGroup):
 class ARK_Preferences_Cameras_RenderQueue(bpy.types.PropertyGroup):
     pass
 
-def UI(preferences, layout):
-    items = (name for name, module in MODULES.items() if hasattr(module, "UI"))
-    for name in items:
-        module = MODULES[name]
-        properties = getattr(preferences, name)
-        layout = layout.box()
-        module.UI(properties, layout)
-    return None
-
 CLASSES = [
     ARK_WindowManager_Cameras_RenderQueue,
     ARK_Preferences_Cameras_RenderQueue,
 ]
 
 def register():
-    utils.bpy.register_modules(MODULES)
     utils.bpy.register_classes(CLASSES)
     return None
 
 def unregister():
     utils.bpy.unregister_classes(CLASSES)
-    utils.bpy.unregister_modules(MODULES)
     return None
