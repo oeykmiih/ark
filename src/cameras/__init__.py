41936f7ed7a401038d069e5814439b7b74c91076
oeykmiih <mail@kyeo.xyz>
2023-10-01 10:18:24 +0200

    fix(cameras): override setter/getter to make sure uilist_index does not highlight any row since we don't use it

--- src/cameras/__init__.py
+++ src/cameras/__init__.py
@@ -335,11 +335,22 @@ class ARK_UL_PROPERTIES_CameraList(bpy.types.UIList):
 
 @addon.property
 class Scene_Interface_Cameras(bpy.types.PropertyGroup):
+    # NOTE: This patches uilist_index to never be changed, meaning it will
+    ## never highlight a row, since we don't use the index in the first place.
+    def set_uilist_index(self, value):
+        self["uilist_index"] = 9999
+        return None
+
+    def get_uilist_index(self):
+        return self.get("uilist_index", 9999)
+
     # NOTE: This index is just to fulfill call requirements,
     ## it's not used in the UIList to search for properties.
     uilist_index : bpy.props.IntProperty(
-        name="",
-        default=999,
+        name = "",
+        default = 9999,
+        set = set_uilist_index,
+        get = get_uilist_index,
     )
 
 @addon.property
