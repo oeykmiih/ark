0b8744dad147ef5b756537426dd607a245d888b1
oeykmiih <mail@kyeo.xyz>
2023-10-03 18:40:04 +0200

    chore: bump version

--- src/__init__.py
+++ src/__init__.py
@@ -5,13 +5,13 @@ bl_info = {
     "description" : "",
     "wiki_url": "",
     "blender" : (3, 3, 0),
-    "version" : (0, 1, 1),
+    "version" : (0, 1, 2),
     "category" : "",
     "location" : "",
     "warning" : "",
 }
 
-__version__ = "0.1.1_dev"
+__version__ = "0.1.2"
 __prefix__ = "ARK"
 
 def import_libraries(libraries):
