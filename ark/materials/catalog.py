89b92f3de6aa3e18430e2894db0f407a1450ae4c
oeykmiih <mail@kyeo.xyz>
2023-11-13 19:40:27 +0100

    fix(catalog): only show material catalog when editor is in 'object' material mode

--- ark/materials/catalog.py
+++ ark/materials/catalog.py
@@ -119,7 +119,8 @@ class ARK_PT_Materials(bpy.types.Panel):
     @classmethod
     def poll(cls, context):
         return (context.space_data.type == 'NODE_EDITOR' and
-            context.space_data.tree_type == 'ShaderNodeTree')
+            context.space_data.tree_type == 'ShaderNodeTree' and
+            context.space_data.shader_type == 'OBJECT',)
 
     def draw(self, context):
         layout = self.layout
