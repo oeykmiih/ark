oeykmiih/ark

commit 7e66d9588ccd40abcc7e60b6329fb63239cdd2ff

refactor(cameras): major refactor, changes include:
- "hierarchy" camera property renamed to "view"
- "hierarchy" class renamed to "structure"
- "blcam" renamed to "bl_cam"
- use pointer for collection, deprecates collection getter function

ark/cameras/__init__.py
@@ -312,8 +312,8 @@ class ARK_PT_PROPERTIES_Scene(bpy.types.Panel):
             elif not bl_cam:
                 utils.bpy.ui.label(info, text="No active camera.")
             else:
-                if not view_combinations.collection_hierarchy.audit(bl_cam, preferences):
-                    renamed = view_combinations.collection_hierarchy.audit_previous(bl_cam, preferences)
+                if not view_combinations.structure.audit(bl_cam, preferences):
+                    renamed = view_combinations.structure.audit_previous(bl_cam, preferences)
                     text = "%s" % "Camera was renamed, sync hierarchy?" if renamed else "Missing camera hierarchy, fix it?"
                     info.alert = True
                     info.operator(

ark/cameras/view_combinations.py
@@ -4,18 +4,24 @@ import bpy
 from ark import utils
 addon = utils.bpy.Addon()
 
-def update(blcam, preferences):
-    update_collection_combination(blcam, preferences)
+def add(bl_cam, context, preferences):
+    structure.create(bl_cam, preferences)
     return None
 
-def update_collection_combination(blcam, preferences):
-    update_collection_combination_props(
-        bpy.context.view_layer.layer_collection.children[preferences.container_viewcombinations],
-        [f"VC:{blcam.name}",],
+def update(bl_cam, context, preferences):
+    update_collections(bl_cam, context, preferences)
+    return None
+
+def update_collections(bl_cam, context, preferences):
+    pr_cam = getattr(bl_cam.data, addon.name)
+
+    update_collection_props(
+        context.view_layer.layer_collection.children[preferences.container_viewcombinations],
+        [pr_cam.view.props.name],
     )
     return None
 
-def update_collection_combination_props(container, target):
+def update_collection_props(container, target):
     for bllaycol in container.children:
         if bllaycol.name in target:
             bllaycol.exclude = False
@@ -23,69 +29,61 @@ def update_collection_combination_props(container, target):
             bllaycol.exclude = True
     return None
 
-class collection_hierarchy():
+def cleanse(bl_cam):
+    pr_cam = getattr(bl_cam.data, addon.name)
+    pr_cam.view.props = None
+    return None
+
+class structure():
     @staticmethod
-    def create(blcam, preferences):
-        pr_cam = getattr(blcam.data, addon.name)
+    def create(bl_cam, preferences):
+        pr_cam = getattr(bl_cam.data, addon.name)
 
         blcol_viewcombinations = utils.bpy.col.obt(preferences.container_viewcombinations, force=True)
-        pr_cam.hierarchy.props = utils.bpy.col.obt(f"VC:{blcam.name}", force=True, parent=blcol_viewcombinations)
+        pr_cam.view.props = utils.bpy.col.obt(f"VC:{bl_cam.name}", force=True, parent=blcol_viewcombinations)
         return None
 
     @staticmethod
-    def update(blcam, preferences):
-        name = blcam.name
-        pr_cam = getattr(blcam.data, addon.name)
+    def update(bl_cam, preferences):
+        name = bl_cam.name
+        pr_cam = getattr(bl_cam.data, addon.name)
 
         blcol_viewcombinations = utils.bpy.col.obt(preferences.container_viewcombinations, force=True)
-        cam_props = pr_cam.hierarchy.props
+        cam_props = pr_cam.view.props
 
         cam_props.name = f"VC:{name}"
         return None
 
     @staticmethod
-    def remove(blcam, preferences):
-        pr_cam = getattr(blcam.data, addon.name)
+    def remove(bl_cam, preferences):
+        pr_cam = getattr(bl_cam.data, addon.name)
 
         blcol_viewcombinations = utils.bpy.col.obt(preferences.container_viewcombinations, force=True)
-        cam_props = pr_cam.hierarchy.props
+        cam_props = pr_cam.view.props
 
         utils.bpy.col.empty(cam_props, objects=True)
         blcol_viewcombinations.children.unlink(cam_props)
         return None
 
     @staticmethod
-    def audit(blcam, preferences):
-        pr_cam = getattr(blcam.data, addon.name)
+    def audit(bl_cam, preferences):
+        pr_cam = getattr(bl_cam.data, addon.name)
 
         conditions = [
             utils.bpy.col.obt(preferences.container_viewcombinations),
-            utils.bpy.col.obt(pr_cam.hierarchy.props.name, local=True),
+            utils.bpy.col.obt(pr_cam.view.props.name, local=True),
         ]
         return all(conditions)
 
     @staticmethod
-    def audit_previous(blcam, preferences):
-        pr_cam = getattr(blcam.data, addon.name)
+    def audit_previous(bl_cam, preferences):
+        pr_cam = getattr(bl_cam.data, addon.name)
 
         conditions = [
             utils.bpy.col.obt(preferences.container_viewcombinations),
-            utils.bpy.col.obt(pr_cam.hierarchy.props.name, local=True),
+            utils.bpy.col.obt(pr_cam.view.props.name, local=True),
         ]
         return all(conditions)
-
-    @staticmethod
-    def save_refs(blcam):
-        pr_cam = getattr(blcam.data, addon.name)
-        name = blcam.name
-        pr_cam.hierarchy.props = f"VC:{name}"
-        return None
-
-    @staticmethod
-    def cleanse_refs(blcam):
-        pr_cam = getattr(blcam.data, addon.name)
-        pr_cam.hierarchy.props = ""
-        return None
 
 @addon.property
 class WindowManager_Cameras_ViewCombinations(bpy.types.PropertyGroup):

ark/cameras/common.py
@@ -8,32 +8,23 @@ addon = utils.bpy.Addon()
 from . import properties
 from . import view_combinations
 
-def add_camera_hierarchy(blcam, preferences, renamed=False):
-    if renamed:
-        view_combinations.collection_hierarchy.update(blcam, preferences)
-        view_combinations.update(blcam, preferences)
-    else:
-        view_combinations.collection_hierarchy.create(blcam, preferences)
-    view_combinations.collection_hierarchy.save_refs(blcam)
-    return None
-
-def set_camera_active(blcam, context, preferences):
-    context.scene.camera = blcam
+def set_camera_active(bl_cam, context, preferences):
+    context.scene.camera = bl_cam
 
     blcol_cameras = utils.bpy.col.obt(preferences.container_cameras, local=True)
 
-    obt_camera_tracker(preferences.trackers_camera, blcam, blcol_cameras)
-    update_camera_properties(blcam, context, preferences)
+    obt_camera_tracker(preferences.trackers_camera, bl_cam, blcol_cameras)
+    update_camera_properties(bl_cam, context, preferences)
     return None
 
 def get_next_camera(container):
     if container is None:
         return None
-    blcam = None
+    bl_cam = None
     for obj in container.all_objects:
         if obj.type == 'CAMERA':
-            blcam = obj
-    return blcam
+            bl_cam = obj
+    return bl_cam
 
 def set_next_camera_active(context, preferences):
     next_cam = get_next_camera(utils.bpy.col.obt(preferences.container_cameras))
@@ -41,20 +32,20 @@ def set_next_camera_active(context, preferences):
         set_camera_active(next_cam, context, preferences)
     return None
 
-def obt_camera_tracker(tracker_name, blcam, blcol):
+def obt_camera_tracker(tracker_name, bl_cam, blcol):
     tracker = utils.bpy.obj.obt(tracker_name, parent=blcol, force=True, local=True)
-    tracker.matrix_world = blcam.matrix_world
+    tracker.matrix_world = bl_cam.matrix_world
 
     tracker.hide_select = True
     tracker.hide_select = True
     tracker.hide_viewport = True
     return None
 
-def update_camera_properties(blcam, context, preferences):
-    pr_cam = getattr(blcam.data, addon.name)
+def update_camera_properties(bl_cam, context, preferences):
+    pr_cam = getattr(bl_cam.data, addon.name)
     properties.Camera.update_exposure(pr_cam, context)
     properties.Camera.update_resolution(pr_cam, context)
-    view_combinations.update(blcam, context, preferences)
+    view_combinations.update(bl_cam, context, preferences)
     return None
 
 def add_camera(context, preferences):
@@ -68,7 +59,7 @@ def add_camera(context, preferences):
         overwrite='NEW',
     )
 
-    blcam = utils.bpy.obj.obt(
+    bl_cam = utils.bpy.obj.obt(
         name,
         data=bldata,
         force=True,
@@ -76,49 +67,49 @@ def add_camera(context, preferences):
         overwrite='NEW',
     )
 
-    force_camera_verticals(blcam)
-    add_camera_hierarchy(blcam, preferences)
-    set_camera_active(blcam, context, preferences)
+    force_camera_verticals(bl_cam)
+    view_combinations.add(bl_cam, context, preferences)
+    set_camera_active(bl_cam, context, preferences)
     return None
 
-def duplicate_camera(blcam, context, preferences):
+def duplicate_camera(bl_cam, context, preferences):
     name = preferences.default_name
     blcol_cameras = utils.bpy.col.obt(preferences.container_cameras)
 
-    new_cam = blcam.copy()
-    new_cam.data = blcam.data.copy()
-    view_combinations.collection_hierarchy.cleanse_refs(new_cam)
+    new_cam = bl_cam.copy()
+    new_cam.data = bl_cam.data.copy()
+    view_combinations.structure.cleanse(new_cam)
 
     blcol_cameras.objects.link(new_cam)
 
-    add_camera_hierarchy(new_cam, preferences)
+    view_combinations.add(new_cam, context, preferences)
     set_camera_active(new_cam, context, preferences)
     return None
 
-def remove_camera(blcam, context, preferences):
-    if view_combinations.collection_hierarchy.audit(blcam, preferences):
-        view_combinations.collection_hierarchy.remove(blcam, preferences)
+def remove_camera(bl_cam, context, preferences):
+    if view_combinations.structure.audit(bl_cam, preferences):
+        view_combinations.structure.remove(bl_cam, preferences)
 
-    utils.bpy.obj.remove(blcam, purge_data=True)
+    utils.bpy.obj.remove(bl_cam, purge_data=True)
     return None
 
-def remove_cameras(blcams, context, preferences):
-    for blcam in blcams:
-        remove_camera(blcam, context, preferences)
+def remove_cameras(bl_cams, context, preferences):
+    for bl_cam in bl_cams:
+        remove_camera(bl_cam, context, preferences)
     return None
 
-def force_camera_verticals(blcam):
-    _rotation_mode = blcam.rotation_mode
-    blcam.rotation_mode = 'XYZ'
-    blcam.rotation_euler[0] = math.radians(90)
-    blcam.rotation_euler[1] = math.radians(0)
-    blcam.rotation_mode = _rotation_mode
+def force_camera_verticals(bl_cam):
+    _rotation_mode = bl_cam.rotation_mode
+    bl_cam.rotation_mode = 'XYZ'
+    bl_cam.rotation_euler[0] = math.radians(90)
+    bl_cam.rotation_euler[1] = math.radians(0)
+    bl_cam.rotation_mode = _rotation_mode
     return None
 
-def audit_camera_verticals(blcam):
+def audit_camera_verticals(bl_cam):
     conditions = [
-        math.isclose(blcam.rotation_euler[0], math.radians(90), rel_tol=0.1),
-        math.isclose(blcam.rotation_euler[1], math.radians(0), rel_tol=0.1),
+        math.isclose(bl_cam.rotation_euler[0], math.radians(90), rel_tol=0.1),
+        math.isclose(bl_cam.rotation_euler[1], math.radians(0), rel_tol=0.1),
     ]
     return all(conditions)
 

ark/cameras/properties.py
@@ -8,11 +8,13 @@ addon = utils.bpy.Addon()
 from . import defaults
 from . import enums
 
-class Camera_Hierarchy(bpy.types.PropertyGroup):
-    props : bpy.props.StringProperty()
+class Camera_View(bpy.types.PropertyGroup):
+    props : bpy.props.PointerProperty(
+        type=bpy.types.Collection,
+    )
 
 class Camera(bpy.types.PropertyGroup):
-    hierarchy : bpy.props.PointerProperty(type=Camera_Hierarchy)
+    view : bpy.props.PointerProperty(type=Camera_View)
 
     def set_aperture(self, value):
         self["aperture"] = value
@@ -190,7 +192,7 @@ class Camera(bpy.types.PropertyGroup):
     )
 
 CLASSES = [
-    Camera_Hierarchy,
+    Camera_View,
     Camera,
 ]
 

ark/cameras/render_queue.py
@@ -52,8 +52,8 @@ class ARK_OT_RenderQueue(bpy.types.Operator):
     slots : bpy.props.BoolProperty()
     export : bpy.props.BoolProperty()
 
-    def set_filepath(self, context, blcam):
-        pr_cam = getattr(blcam.data, addon.name)
+    def set_filepath(self, context, bl_cam):
+        pr_cam = getattr(bl_cam.data, addon.name)
 
         # NOTE: Make fileoutput blender folder if file output is empty
         dirpath = "//" if self._fpath == "" else self._fpath

commit ed4145d68642d650fa58045da87984ccde7733f2

fix(cameras): camera hierarchy renaming was broken by: 7e66d9588ccd40abcc7e60b6329fb63239cdd2ff

ark/cameras/__init__.py
@@ -36,8 +36,8 @@ class ARK_OT_CreateArkHierarchy(bpy.types.Operator):
         ark_hierarchy.create(preferences, context)
         return {'FINISHED'}
 
-class ARK_OT_AddCameraHierarchy(bpy.types.Operator):
-    bl_idname = f"{addon.name}.add_camera_hierarchy"
+class ARK_OT_CreateCameraHierarchy(bpy.types.Operator):
+    bl_idname = f"{addon.name}.create_camera_hierarchy"
     bl_label = ""
     bl_options = {'UNDO', 'INTERNAL'}
 
@@ -47,7 +47,15 @@ class ARK_OT_AddCameraHierarchy(bpy.types.Operator):
         view_combinations.structure.create(context.scene.camera, addon.preferences)
         return {'FINISHED'}
 
-        common.add_camera_hierarchy(bl_cam, preferences, renamed=self.renamed)
+class ARK_OT_UpdateCameraHierarchy(bpy.types.Operator):
+    bl_idname = f"{addon.name}.add_camera_hierarchy"
+    bl_label = ""
+    bl_options = {'UNDO', 'INTERNAL'}
+
+    renamed : bpy.props.BoolProperty(default = False)
+
+    def execute(self, context):
+        view_combinations.structure.update(context.scene.camera, addon.preferences)
         return {'FINISHED'}
 
 class ARK_OT_SetCameraActive(bpy.types.Operator):
@@ -309,13 +317,12 @@ class ARK_PT_PROPERTIES_Scene(bpy.types.Panel):
                 utils.bpy.ui.label(info, text="No active camera.")
             else:
                 if not view_combinations.structure.audit(bl_cam, preferences):
-                    renamed = view_combinations.structure.audit_previous(bl_cam, preferences)
-                    text = "%s" % "Camera was renamed, sync hierarchy?" if renamed else "Missing camera hierarchy, fix it?"
-                    info.alert = True
-                    info.operator(
-                        ARK_OT_AddCameraHierarchy.bl_idname,
-                        text = text,
-                    ).renamed = renamed
+                    if view_combinations.structure.audit_previous(bl_cam, preferences):
+                        info.alert = True
+                        info.operator(ARK_OT_UpdateCameraHierarchy.bl_idname, text="Camera was renamed, sync hierarchy?")
+                    else:
+                        info.alert = True
+                        info.operator(ARK_OT_CreateCameraHierarchy.bl_idname, text="Missing camera collections, fix it?")
                 else:
                     info.label(text="")
 
@@ -560,7 +567,8 @@ def UI(preferences, layout):
 
 CLASSES = [
     ARK_OT_CreateArkHierarchy,
-    ARK_OT_AddCameraHierarchy,
+    ARK_OT_CreateCameraHierarchy,
+    ARK_OT_UpdateCameraHierarchy,
     ARK_OT_SetCameraActive,
     ARK_OT_AddCamera,
     ARK_OT_DuplicateCamera,

ark/cameras/view_combinations.py
@@ -51,13 +51,8 @@ class structure():
 
     @staticmethod
     def update(bl_cam, preferences):
-        name = bl_cam.name
         pr_cam = getattr(bl_cam.data, addon.name)
-
-        blcol_viewcombinations = utils.bpy.col.obt(preferences.container_viewcombinations, force=True)
-        cam_props = pr_cam.view.props
-
-        cam_props.name = f"VC:{name}"
+        pr_cam.view.props.name = f"VC:{bl_cam.name}"
         return None
 
     @staticmethod
@@ -75,21 +70,28 @@ class structure():
     def audit(bl_cam, preferences):
         pr_cam = getattr(bl_cam.data, addon.name)
 
-        conditions = [
-            utils.bpy.col.obt(preferences.container_viewcombinations),
-            utils.bpy.col.obt(pr_cam.view.props.name, local=True),
-        ]
-        return all(conditions)
+        if pr_cam.view.props is None:
+            return False
+        else:
+            blcol_viewcombinations = utils.bpy.col.obt(preferences.container_viewcombinations)
+            conditions = [
+                pr_cam.view.props.name[3:] == bl_cam.name,
+                pr_cam.view.props.name in blcol_viewcombinations.children,
+            ]
+            return all(conditions)
 
     @staticmethod
     def audit_previous(bl_cam, preferences):
         pr_cam = getattr(bl_cam.data, addon.name)
 
-        conditions = [
-            utils.bpy.col.obt(preferences.container_viewcombinations),
-            utils.bpy.col.obt(pr_cam.view.props.name, local=True),
-        ]
-        return all(conditions)
+        if pr_cam.view.props is None:
+            return False
+        else:
+            conditions = [
+                utils.bpy.col.obt(preferences.container_viewcombinations),
+                utils.bpy.col.obt(pr_cam.view.props.name, local=True),
+            ]
+            return all(conditions)
 
 @addon.property
 class WindowManager_Cameras_ViewCombinations(bpy.types.PropertyGroup):