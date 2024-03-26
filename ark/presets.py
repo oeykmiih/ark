91a68506acd0d0003fb727330a16fa2257093471
oeykmiih <mail@kyeo.xyz>
2024-03-26 11:00:00 +0000

    feature(presets): expose copy of viewport settings

--- ark/presets.py
+++ ark/presets.py
@@ -21,9 +21,9 @@ class ARK_OT_CopyPreferences(bpy.types.Operator):
         return {'FINISHED'}
 
 class ARK_OT_VIEW3D_CopyViewportSettings(bpy.types.Operator):
-    """Copies viewport settings to clipboard."""
+    """Copies viewport settings to clipboard"""
     bl_idname = "ark.copy_viewport_settings"
-    bl_label = "Copy View3D Settings"
+    bl_label = "Copy Viewport Settings"
 
     @classmethod
     def poll(cls, context):
@@ -42,8 +42,15 @@ class ARK_OT_VIEW3D_CopyViewportSettings(bpy.types.Operator):
         self.layout.operator(__class__.bl_idname, text=__class__.bl_label)
         return None
 
+def ARK_MT_VIEW3D_TransferViewportSettings(self, context):
+    layout = self.layout
+    layout.separator()
+    layout.operator(ARK_OT_VIEW3D_CopyViewportSettings.bl_idname, text=ARK_OT_VIEW3D_CopyViewportSettings.bl_label)
+    layout.operator(ARK_OT_ApplyJSONProperties.bl_idname, text="Paste Viewport Settings")
+    return None
+
 class ARK_OT_ApplyJSONProperties(bpy.types.Operator):
-    """Applies settings from clipboard."""
+    """Applies settings from clipboard"""
     bl_idname = "ark.apply_json_properties"
     bl_label = "Apply JSON"
 
@@ -69,17 +76,19 @@ class Preferences_Interface_Presets(bpy.types.PropertyGroup):
     pass
 
 CLASSES = [
-    ARK_OT_VIEW3D_CopyViewportSettings,
     ARK_OT_CopyPreferences,
     ARK_OT_ApplyJSONProperties,
+    ARK_OT_VIEW3D_CopyViewportSettings,
     WindowManager_Interface_Presets,
     Preferences_Interface_Presets,
 ]
 
 def register():
     utils.bpy.register_classes(CLASSES)
+    bpy.types.VIEW3D_MT_object_context_menu.append(ARK_MT_VIEW3D_TransferViewportSettings)
     return None
 
 def unregister():
+    bpy.types.VIEW3D_MT_object_context_menu.remove(ARK_MT_VIEW3D_TransferViewportSettings)
     utils.bpy.unregister_classes(CLASSES)
     return None
